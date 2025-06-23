e399ef922c5a81aab84cf5ec710988012363f3e8
Stefan <stefan@coden-lassen.de>
2025-06-23 23:49:58 +0200

    fix: remove cache on edge

--- lib/email/send-email.ts
+++ lib/email/send-email.ts
@@ -1,4 +1,3 @@
-
 export interface SendEmailParams {
   replyTo: string;
   replyToName: string;
@@ -6,9 +5,21 @@ export interface SendEmailParams {
   text: string;
 }
 
-export async function sendEmail({ replyTo, replyToName, subject, text }: SendEmailParams): Promise<void> {
+export async function sendEmail({
+  replyTo,
+  replyToName,
+  subject,
+  text,
+}: SendEmailParams): Promise<void> {
+  const url =
+    process.env.NODE_ENV === "production"
+      ? "https://services.coden-lassen.de/send-email"
+      : "http://localhost:8000/send-email";
 
-  const url = "https://services.coden-lassen.de/send-email"
+  const apiKey =
+    process.env.NODE_ENV === "production"
+      ? process.env.CODEN_LASSEN_SERVICES_API_KEY!
+      : "abc";
 
   const payload: {
     subject: string;
@@ -19,35 +30,41 @@ export async function sendEmail({ replyTo, replyToName, subject, text }: SendEma
     subject,
     message: text,
     reply_to: replyTo,
-    reply_to_name: replyToName
-  }
+    reply_to_name: replyToName,
+  };
 
   try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'x-api-key': process.env.CODEN_LASSEN_SERVICES_API_KEY!,
-        },
-        cache: "no-store",
-        body: JSON.stringify(payload),
-      });
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "x-api-key": apiKey,
+      },
+      body: JSON.stringify(payload),
+    });
 
-      console.log({status: response.status, statusText: response.statusText})
-    
-      if (!response.ok) {
-        const errorText = await response.text();
-        console.log({errorText})
-        throw new Error(errorText || "unknown error");
-      }
+    console.log({ status: response.status, statusText: response.statusText });
 
-      const data = await response.json();
-      console.log(data)
-      return data;
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.log({ errorText });
+      throw new Error(errorText || "unknown error");
+    }
 
+    const data = await response.json();
+    console.log(data);
+    return data;
   } catch (error) {
-    console.log(`Failed to send email: ${error instanceof Error ? error.message : String(error)}`)
-    throw new Error(`Failed to send email: ${error instanceof Error ? error.message : String(error)}`);
+    console.log(
+      `Failed to send email: ${
+        error instanceof Error ? error.message : String(error)
+      }`
+    );
+    throw new Error(
+      `Failed to send email: ${
+        error instanceof Error ? error.message : String(error)
+      }`
+    );
   }
 }
