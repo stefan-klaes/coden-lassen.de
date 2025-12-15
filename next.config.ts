fff5a1221a0956695fa671951cfb3ef6b7e06959
Stefan Klaes <stefan@coden-lassen.de>
2025-12-15 23:27:46 +0100

    build: worker config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
